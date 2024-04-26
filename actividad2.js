43caf7a2a22a85760358d5b3976c40f834f62a71
cristhiandona <cristhiandona@gmail.com>
2024-04-25 21:04:17 -0500

    Ultimos Cambios

--- actividad2.js
+++ actividad2.js
@@ -1,7 +1,7 @@
 /*Estudiantes:
 Alejandra Granada
 Cristhian Aguirre Cardona
-2. Factorial:Implementa una función que calcule el factorial de un númerodado. 
+Ejercicio 2. Factorial:Implementa una función que calcule el factorial de un númerodado. 
 Puedes usar cualquier tipo de función.
 */
 
